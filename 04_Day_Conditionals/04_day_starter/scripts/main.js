9faedcb0b0707b06f44d95e4ccfca00474b2505e
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-05-21 14:24:11 +0500

    Add the level-2 question solution

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -29,3 +29,81 @@ console.log(
     ? `${number} is an even number.`
     : `${number} is a odd number.`
 );
+
+// Exercise Level - 2
+// Quesion - 1
+/* Write a code which can give grades to students according to theirs scores:
+80-100, A
+70-89, B
+60-69, C
+50-59, D
+0-49, F
+*/
+
+score = prompt("Enter your score: ");
+switch (true) {
+  case 80 <= score && score <= 100:
+    console.log("Your Grade is A.");
+    break;
+  case 70 <= score && score < 80:
+    console.log("Your Grade is B.");
+    break;
+  case 60 <= score && score < 70:
+    console.log("Your Grade is C.");
+    break;
+  case 50 <= score && score < 60:
+    console.log("Your Grade is D.");
+    break;
+  default:
+    console.log("Your Grade is F.");
+}
+
+// Question - 2
+/* Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer
+*/
+month = prompt("Enter the month");
+month = month.toLowerCase();
+console.log(month);
+switch (true) {
+  case month === "september" || month === "october" || month === "november":
+    console.log("The season is Autumn.");
+    break;
+  case month === "december" || month === "january" || month === "february":
+    console.log("The season is Winter.");
+    break;
+  case month === "march" || month === "april" || month === "May":
+    console.log("The season is Spring.");
+    break;
+  case month === "june" || month === "july" || month === "august":
+    console.log("The season is Summer.");
+    break;
+  default:
+    console.log("Your choice is wrong!!");
+}
+
+// Question - 3
+/* 
+Check if a day is weekend day or a working day. Your script will take day as an input.
+    What is the day  today? Saturday
+    Saturday is a weekend.
+
+    What is the day today? saturDaY
+    Saturday is a weekend.
+
+    What is the day today? Friday
+    Friday is a working day.
+
+    What is the day today? FrIDAy
+    Friday is a working day.
+*/
+
+day = prompt("What is the day   today? ").toLowerCase();
+console.log(
+  day === "saturday" || day === "sunday"
+    ? `${day} is a weekend.`
+    : `${day} is a working day.`
+);
