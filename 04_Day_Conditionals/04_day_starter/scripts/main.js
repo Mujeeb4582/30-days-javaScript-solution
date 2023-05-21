20b362c7d825c95cb4008ddd3ebb324bdeb18d63
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-05-21 12:55:30 +0500

    Add the exercise 1 first two question solution

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,13 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+// alert('Open the browser console whenever you work on JavaScript')
+// **************************Exercise**********************************
+// **************************Level - 1 ********************************
+
+age = prompt("Enter your age: ");
+if (age >= 18) console.log("You are old enough to drive.");
+else console.log(`You are left with ${18 - age} years to drive.`);
+
+if (age === 30) console.log("We have same age.");
+else if (age > 30) console.log(`You are ${age - 30} year older then me.`);
+else console.log(`You are ${30 - age} year younger then me.`);
